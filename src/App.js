2f406041519863baad7f2667a1bdd8753a1fc4db
101486171 <101486171@georgebrown.ca>
2023-10-04 14:04:03 -0400

    upload file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Greeter from './Greeter';
+
+function App() {
+  return (
+    <div>
+      <h1> Hello, React!</h1>
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
